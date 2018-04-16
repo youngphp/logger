a0b448d1515e31c4719cc559f3f90e874f359787
youngphp <soul_521000@live.com>
2018-04-16 16:27:42 +0800

    update .md

--- logger_test.go
+++ logger_test.go
@@ -7,9 +7,10 @@ func TestFileLogger(t *testing.T) {
 	log.Debug("the use is %s age is %d", "fei", 20)
 	log.Warn("the warn is %s", "open file is error")
 	log.Fatal("program is die %s", "程序崩溃了")
-
+	log.Close()
 	log = NewConsoleLogger(DebugLevel)
 	log.Debug("the use is %s age is %d", "fei", 20)
 	log.Warn("the warn is %s", "open file is error")
 	log.Fatal("program is die %s", "程序崩溃了")
+	log.Close()
 }
